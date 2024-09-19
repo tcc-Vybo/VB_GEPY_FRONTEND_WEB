b904c878a8a01809fbb73ffce8863cda55240297
phfeba <phfeba@hotmail.com>
2024-09-19 20:46:32 -0300

    update

--- src/Components/Alunos/index.jsx
+++ src/Components/Alunos/index.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./style.css";
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import './style.css';
+import { Outlet } from 'react-router-dom';
 
 export default function Alunos() {
   return (
@@ -8,8 +8,6 @@ export default function Alunos() {
       <header className="header-alunos">
         <button>Cadastrar Alunos</button>
         <button>Buscar Alunos</button>
-        <button>Alterar Alunos</button>
-        <button>Deletar Alunos</button>
       </header>
       <Outlet />
     </div>
