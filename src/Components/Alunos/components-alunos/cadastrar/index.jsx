23df14bc40cc23d06b99ae406423a6490bd17f09
phfeba <phfeba@hotmail.com>
2024-09-19 19:50:00 -0300

    child components

---
+++ src/Components/Alunos/components-alunos/cadastrar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function Alunos() {
+  return (
+    <div className="container-principal">
+      <h1>Alunos</h1>
+      <p>oiiiii</p>
+    </div>
+  );
+}
