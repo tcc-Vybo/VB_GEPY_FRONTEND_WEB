a5c6010ad8c937c727779273ba991955249ff0a8
Gabriela Chagas <gabichagassantos@hotmail.com>
2024-09-19 19:59:17 -0300

    update

--- src/Components/Alunos/components-alunos/cadastrar/index.jsx
+++ src/Components/Alunos/components-alunos/cadastrar/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-export default function CadastroAlunos() {
+export default function CadastrarAlunos() {
   return (
     <div className="container-principal">
       <h1>Cadastro</h1>
