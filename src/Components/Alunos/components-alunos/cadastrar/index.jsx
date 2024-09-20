cd02fb004269bf7fab2522321e2479ada69ccda4
Pedro Feba <phfeba@hotmail.com>
2024-09-20 00:19:46 -0300

    update

--- src/Components/Alunos/components-alunos/cadastrar/index.jsx
+++ src/Components/Alunos/components-alunos/cadastrar/index.jsx
@@ -1,85 +1,127 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function CadastrarAlunos() {
   return (
-    <div className="container-alunos">
-      <div className="container-secundario">
-        <h1>Identificação</h1>
-        <div className="inputs-identificacao">
-          <div className="input-cadastro">
+    <div className='container-alunos'>
+      <div className='container-secundario'>
+        <div className='inputs-identificacao'>
+          <div className='input-cadastro tipo-um'>
             <p>Nome Completo</p>
-            <input type="text" />
+            <input type='text' />
           </div>
-          <div className="input-cadastro">
-            <p>Data de Nascimento</p>
-            <input type="text" />
+          <div className='input-cadastro tipo-dois  '>
+            <p>Data de Nasc</p>
+            <input type='text' />
           </div>
-          <div className="input-cadastro">
-            <p>Cidade de Nascimento</p>
-            <input type="text" />
+          <div className='input-cadastro tipo-dois'>
+            <p>Cidade de Nasc</p>
+            <input type='text' />
           </div>
-          <div className="input-cadastro">
-            <p>UF de Nascimento</p>
-            <input type="text" />
+          <div className='input-cadastro tipo-dois'>
+            <p>UF de Nasc</p>
+            <input type='text' />
+          </div>
+          <div className='input-cadastro tipo-dois'>
+            <p>UF de Nasc</p>
+            <input type='text' />
           </div>
         </div>
-        <div className="inputs-identificacao">
-          <div className="input-cadastro">
+        <div className='inputs-identificacao '>
+          <div className='input-cadastro'>
             <p>Nacionalidade</p>
-            <input type="text" />
+            <input type='text' />
           </div>
-          <div className="input-cadastro">
+          <div className='input-cadastro tipo-dois'>
             <p>Gênero</p>
-            <input type="text" />
+            <input type='text' />
           </div>
-          <div className="input-cadastro">
+          <div className='input-cadastro tipo-dois'>
             <p>Cor/Raça</p>
-            <input type="text" />
+            <input type='text' />
           </div>
-          <div className="input-cadastro">
+          <div className='input-cadastro tipo-um'>
             <p>Necessidades Especiais</p>
-            <input type="text" />
+            <input type='text' />
           </div>
         </div>
-        <h1>Endereço Residencial</h1>
-        <div className="inputs-identificacao">
-          <div className="input-cadastro">
+        <div className='inputs-identificacao'>
+          <div className='input-cadastro tipo-dois'>
             <p>CEP</p>
-            <input type="text" />
+            <input type='text' />
           </div>
-          <div className="input-cadastro">
+          <div className='input-cadastro tipo-um'>
             <p>Endereço</p>
-            <input type="text" />
+            <input type='text' />
           </div>
-          <div className="input-cadastro">
+          <div className='input-cadastro tipo-dois'>
             <p>Nº</p>
-            <input type="text" />
+            <input type='text' />
           </div>
-          <div className="input-cadastro">
+          <div className='input-cadastro tipo-dois'>
             <p>Complemento</p>
-            <input type="text" />
+            <input type='text' />
           </div>
         </div>
-        <div className="inputs-identificacao">
-          <div className="input-cadastro">
+        <div className='inputs-identificacao'>
+          <div className='input-cadastro tipo-um'>
             <p>Bairro</p>
-            <input type="text" />
+            <input type='text' />
           </div>
-          <div className="input-cadastro">
+          <div className='input-cadastro tipo-um'>
             <p>Município</p>
-            <input type="text" />
+            <input type='text' />
           </div>
-          <div className="input-cadastro">
+          <div className='input-cadastro tipo-dois'>
             <p>UF</p>
-            <input type="text" />
+            <input type='text' />
+          </div>
+        </div>
+        <div className='inputs-identificacao'>
+          <div className='input-cadastro tipo-dois'>
+            <p>RG</p>
+            <input type='text' />
+          </div>
+          <div className='input-cadastro tipo-dois'>
+            <p>CPF</p>
+            <input type='text' />
+          </div>
+          <div className='input-cadastro tipo-dois'>
+            <p>Dt de Emissão</p>
+            <input type='text' />
+          </div>
+          <div className='input-cadastro tipo-dois'>
+            <p>Orgão Expeditor</p>
+            <input type='text' />
+          </div>
+        </div>
+        <div className='inputs-identificacao'>
+          <div className='input-cadastro tipo-dois'>
+            <p>Tel. Aluno</p>
+            <input type='text' />
+          </div>
+          <div className='input-cadastro tipo-um'>
+            <p>Nome do Responsavel</p>
+            <input type='text' />
+          </div>
+          <div className='input-cadastro tipo-um'>
+            <p>CPF do Responsavel</p>
+            <input type='text' />
+          </div>
+          <div className='input-cadastro tipo-dois'>
+            <p>Relação</p>
+            <input type='text' />
+          </div>
+          <div className='input-cadastro tipo-dois'>
+            <p>Tel. Responsavel</p>
+            <input type='text' />
           </div>
         </div>
       </div>
-      <div className="alunos-button">
-          <button>Voltar</button>
-          <button>Salvar</button>
-        </div>
+      <div className='alunos-button'>
+        <button>Voltar</button>
+        <button>Salvar</button>
+      </div>
     </div>
   );
 }
