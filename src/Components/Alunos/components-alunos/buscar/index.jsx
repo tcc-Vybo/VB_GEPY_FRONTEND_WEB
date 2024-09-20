ae62845954890d74b43541a5767df1910a44ff37
Gabriela Chagas <gabichagassantos@hotmail.com>
2024-09-19 22:04:03 -0300

    update

--- src/Components/Alunos/components-alunos/buscar/index.jsx
+++ src/Components/Alunos/components-alunos/buscar/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 export default function BuscarAlunos() {
   return (
     <div className="container-alunos">
       <div className="container-secundario">
-        <h1>a</h1>
+        <div className="pesquisa">
+          <label className="label-buscar">Nome do Aluno: </label>
+          <input
+            type="text"
+            placeholder="Digite o Nome do Aluno"
+            className="input-buscar"
+          ></input>
+        </div>
+        <div className="resultados"></div>
       </div>
       <div className="alunos-button">
         <button>Voltar</button>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
-import Footer from "../../Components/Footer";
+import "./style.css";
 import Navbar from "../../Components/Navbar";
 import Sidebar from "../../Components/Sidebar";
-import Logo from "../../Assets/Images/Logo.png";
-import "./style.css";
+import Footer from "../../Components/Footer";
+import InitialButtons from "../../Components/Card";
+import { Outlet } from "react-router-dom";
+import Alunos from "../../Components/Alunos";
+import Turmas from "../../Components/Turmas";
 
 export default function Home() {
+  const alunos = true;
   return (
     <div className="App">
       <Navbar />
       <div className="container-row">
         <Sidebar />
         <div className="container-principal">
-          <div className="mensagem-bemvindo">
-            <img src={Logo} className="logo" />
-            <h1 className="bemvindo">Bem vindo, Diretor!</h1>
-          </div>
           <Outlet />
         </div>
       </div>
