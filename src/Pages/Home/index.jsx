808f9e0f6de695b99e5dc5ac1d93d8035bf932fb
phfeba <phfeba@hotmail.com>
2024-09-19 22:05:51 -0300

    update

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,15 +1,11 @@
-import React from "react";
-import "./style.css";
-import Navbar from "../../Components/Navbar";
-import Sidebar from "../../Components/Sidebar";
-import Footer from "../../Components/Footer";
-import InitialButtons from "../../Components/Card";
-import { Outlet } from "react-router-dom";
-import Alunos from "../../Components/Alunos";
-import Turmas from "../../Components/Turmas";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../../Components/Footer';
+import Navbar from '../../Components/Navbar';
+import Sidebar from '../../Components/Sidebar';
+import './style.css';
 
 export default function Home() {
-  const alunos = true;
   return (
     <div className="App">
       <Navbar />
