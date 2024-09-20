0d078e99d66006b187116b6afc7ae73804033edc
Guilherme Rosa <147450461+guipereirar@users.noreply.github.com>
2024-09-19 21:58:19 -0300

    update

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from '../../Components/Footer';
-import Navbar from '../../Components/Navbar';
-import Sidebar from '../../Components/Sidebar';
-import './style.css';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../../Components/Footer";
+import Navbar from "../../Components/Navbar";
+import Sidebar from "../../Components/Sidebar";
+import Logo from "../../Assets/Images/Logo.png";
+import "./style.css";
 
 export default function Home() {
   return (
@@ -12,6 +13,10 @@ export default function Home() {
       <div className="container-row">
         <Sidebar />
         <div className="container-principal">
+          <div className="mensagem-bemvindo">
+            <img src={Logo} className="logo" />
+            <h1 className="bemvindo">Bem vindo, Diretor!</h1>
+          </div>
           <Outlet />
         </div>
       </div>
