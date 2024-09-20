4e58cb6ea5c070b1c23afb8b2fec29977656a254
phfeba <phfeba@hotmail.com>
2024-09-19 21:45:47 -0300

    update

--- src/Components/Alunos/components-alunos/cadastrar/index.jsx
+++ src/Components/Alunos/components-alunos/cadastrar/index.jsx
@@ -7,6 +7,10 @@ export default function CadastrarAlunos() {
       <div className="container-secundario">
         <h1>a</h1>
       </div>
+      <div className="alunos-button">
+        <button>Voltar</button>
+        <button>Salvar</button>
+      </div>
     </div>
   );
 }
