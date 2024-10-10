378ee96ad46dfbc1c3a2fe340ba22bf9cc32a629
yurimdeodato <yurisenai2023@gmail.com>
2024-10-09 21:45:17 -0300

    .
    .

--- src/components/sidebar/sidebar-icons/index.jsx
+++ src/components/sidebar/sidebar-icons/index.jsx
@@ -1,16 +1,9 @@
 import "./style.css";
 
-<<<<<<< Updated upstream
 function SidebarIcon({ name, icon }) {
   return (
     <div className="sidebar-icon">
       {icon}
-=======
-function SidebarIcon({ name, source }) {
-  return (
-    <div className="sidebar-icon">
-      <img src={source} alt="" />
->>>>>>> Stashed changes
       <p>{name}</p>
     </div>
   );
