4b7b8267c6c96c081c586364bc89c4a6893153d4
Pedro Feba <phfeba@hotmail.com>
2024-10-16 21:28:35 -0300

    sidebar change

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,16 +4,17 @@ import BuscaTurma from '../../components/busca-turma';
 import CadastroAlunos from '../../components/cadastro-alunos';
 import CadastroFuncionarios from '../../components/cadastro-de-funcionarios';
 import Sidebar from '../../components/sidebar';
+import SidebarAnt from '../../components/sidebar copy';
 import './style.css';
 
 export default function Home() {
   return (
-    <div className="main-page">
-      <div className="sidebar-container">
-        <Sidebar />
+    <div className='main-page'>
+      <div className='sidebar-container'>
+        <SidebarAnt />
       </div>
-      <div className="main-content">
-        <div className="content-container">
+      <div className='main-content'>
+        <div className='content-container'>
           <BuscaTurma />
         </div>
       </div>
