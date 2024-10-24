3413e0b368c1f909a1ee587a415a336b527c6869
phfeba <phfeba@hotmail.com>
2024-10-24 19:16:14 -0300

    update

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,13 +8,13 @@ import './style.css';
 
 export default function Home() {
   return (
-    <div className='main-page'>
-      <div className='sidebar-container'>
+    <div className="main-page">
+      <div className="sidebar-container">
         <Sidebar />
       </div>
-      <div className='main-content'>
-        <div className='content-container'>
-          <BuscaTurma />
+      <div className="main-content">
+        <div className="content-container">
+          <CadastroAlunos />
         </div>
       </div>
     </div>
