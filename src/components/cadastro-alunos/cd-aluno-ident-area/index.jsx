56dc467adb3ec2c15eef0ab6314a9f200ad1ae47
Pedro Feba <phfeba@hotmail.com>
2024-10-30 16:08:52 -0300

    update

--- src/components/cadastro-alunos/cd-aluno-ident-area/index.jsx
+++ src/components/cadastro-alunos/cd-aluno-ident-area/index.jsx
@@ -1,15 +1,15 @@
 import CadastroInput from '../../cd-input';
 
-export default function AlunosInputAreaIdentific() {
+export default function AlunosInputAreaIdentific({ open }) {
   return (
-    <div className="inputs-area">
-      <div className="input-line">
+    <div className={open ? 'inputs-area expanded' : 'inputs-area'}>
+      <div className='input-line'>
         <CadastroInput name={'Nome Completo'} />
         <CadastroInput name={'Data de Nascimento'} />
         <CadastroInput name={'Cidade de Nascimento'} />
         <CadastroInput name={'UF'} />
       </div>
-      <div className="input-line">
+      <div className='input-line'>
         <CadastroInput name={'Nacionalidade'} />
         <CadastroInput name={'Gênero'} />
         <CadastroInput name={'Cor/Raça'} />
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -15,7 +15,7 @@ export default function Home() {
       </div>
       <div className='main-content'>
         <div className='content-container'>
-          <Boletim />
+          <CadastroAlunos />
         </div>
       </div>
     </div>
