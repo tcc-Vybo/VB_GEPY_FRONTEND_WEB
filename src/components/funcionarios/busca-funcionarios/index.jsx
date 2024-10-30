b6a87a2c6c62edb6ac607f201af2b028c636e3a9
Gabriela Chagas <gabichagassantos@hotmail.com>
2024-10-30 20:14:18 -0300

    folders update

--- src/components/funcionarios/busca-funcionarios/index.jsx
+++ src/components/funcionarios/busca-funcionarios/index.jsx
@@ -1,16 +1,13 @@
-import BuscaAlunoLinha from '../busca-alunos/bu-aluno-line';
-import BuscaFuncLinha from './bu-func-line';
-import './style.css';
+import BuscaAlunoLinha from "../alunos/busca-alunos/bu-aluno-line";
+import BuscaFuncLinha from "./bu-func-line";
+import "./style.css";
 
 export default function BuscaFuncionario() {
   return (
     <div className="busca-funcionario">
       <div className="busca-funcionario-header">
         <p>Nome :</p>
-        <input
-          type="text"
-          className="search-funcionarios"
-        />
+        <input type="text" className="search-funcionarios" />
         <p>Função:</p>
         <input type="text" />
         <button>Pesquisar</button>
@@ -20,10 +17,7 @@ export default function BuscaFuncionario() {
           <p className="nome title">Nome</p>
           <p className="funcao title">Responsável</p>
         </div>
-        <BuscaFuncLinha
-          nome={'gusta'}
-          funcao={'techlead'}
-        />
+        <BuscaFuncLinha nome={"gusta"} funcao={"techlead"} />
       </div>
     </div>
   );
