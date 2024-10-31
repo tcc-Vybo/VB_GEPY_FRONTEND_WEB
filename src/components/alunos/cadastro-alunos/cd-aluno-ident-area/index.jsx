890c77f812ea676089a3754d7c1a008fbd110b1f
Pedro Feba <phfeba@hotmail.com>
2024-10-31 00:21:13 -0300

    update

--- src/components/alunos/cadastro-alunos/cd-aluno-ident-area/index.jsx
+++ src/components/alunos/cadastro-alunos/cd-aluno-ident-area/index.jsx
@@ -1,21 +1,20 @@
-import CadastroInput from "../../../cd-input/index";
+import CadastroInput from '../../../cd-input/index';
 
-
-export default function AlunosInputAreaIdentific() {
+export default function AlunosInputAreaIdentific({ open }) {
   return (
-    <div className="inputs-area">
-      <div className="input-line">
-        <CadastroInput name={"Nome Completo"} />
-        <CadastroInput name={"Data de Nascimento"} />
-        <CadastroInput name={"Cidade de Nascimento"} />
-        <CadastroInput name={"UF"} />
+    <div className={open ? 'inputs-area expanded' : 'inputs-area'}>
+      <div className='input-line'>
+        <CadastroInput name={'Nome Completo'} />
+        <CadastroInput name={'Data de Nascimento'} />
+        <CadastroInput name={'Cidade de Nascimento'} />
+        <CadastroInput name={'UF'} />
       </div>
-      <div className="input-line">
-        <CadastroInput name={"Nacionalidade"} />
-        <CadastroInput name={"Gênero"} />
-        <CadastroInput name={"Cor/Raça"} />
-        <CadastroInput name={"Necessidades"} />
-        <CadastroInput name={"Turma"} />
+      <div className='input-line'>
+        <CadastroInput name={'Nacionalidade'} />
+        <CadastroInput name={'Gênero'} />
+        <CadastroInput name={'Cor/Raça'} />
+        <CadastroInput name={'Necessidades'} />
+        <CadastroInput name={'Turma'} />
       </div>
     </div>
   );
