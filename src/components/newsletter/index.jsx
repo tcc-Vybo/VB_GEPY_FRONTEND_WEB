cb910907fc056b5c409e8ce8bd3b9e62fa1f7e19
Gabriela Chagas <gabichagassantos@hotmail.com>
2024-11-07 22:26:17 -0300

    newsletter update

--- src/components/newsletter/index.jsx
+++ src/components/newsletter/index.jsx
@@ -1,54 +1,58 @@
 import "./style.css";
-import Sedutor from "../../assets/sedutores.png"
-import Uniforme from "../../assets/uniforme.png"
+import Sedutor from "../../assets/sedutores.png";
+import Uniforme from "../../assets/uniforme.png";
 
 export default function Newsletter() {
   return (
     <div className="newsletter-partner-container">
       <div className="newsletter-form-left">
-      <h1 className="newsletter-title">Nova notícia</h1>
+        <h1 className="newsletter-title">Nova notícia</h1>
 
-      <label>Título</label>
-      <input type="text"></input>
+        <label>Título</label>
+        <input type="text" className="newsletter-input"></input>
 
-      <label>Subtítulo</label>
-      <input type="text"></input>
+        <label>Subtítulo</label>
+        <input type="text" className="newsletter-input"></input>
 
-      <label>Texto</label>
-      <input type="text"></input>
+        <label>Texto</label>
+        <input type="text" className="newsletter-input-text"></input>
 
-      <button>Selecionar imagem</button>
+        <button className="newsletter-button">Selecionar imagem</button>
       </div>
-
-
 
       <div className="newsletter-preview-right">
-        <h1>Pré Visualização</h1>
+        <h2>Pré Visualização</h2>
         <div className="newsletter-preview-finish">
-        <h2 className="newsletter-title">Novas notícias</h2>
-        <div className="newsletter-news">
-          <img src={Sedutor} className="newsletter-image"></img>
-          <div className="newsletter-preview-text-one">
-        <h2>Cuidado com os sedutores da internet</h2>
-        <br/>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur a laudantium delectus, inventore laborum recusandae blanditiis illum sequi consequatur quia impedit ex dolor, odit voluptate exercitationem placeat nobis, dolore deleniti!</p>
-        </div>
-        </div>
+          <h1 className="newsletter-title">Novas notícias</h1>
+          <div className="newsletter-news">
+            <img src={Sedutor} className="newsletter-image"></img>
+            <div className="newsletter-preview-text-one">
+              <h1>Cuidado com os sedutores da internet</h1>
+              <br />
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Consequuntur a laudantium delectus, inventore laborum recusandae
+                blanditiis illum sequi consequatur quia impedit ex dolor, odit
+                voluptate exercitationem placeat nobis, dolore deleniti!
+              </p>
+            </div>
+          </div>
 
-        <div className="newsletter-news">
-        
-        <div className="newsletter-preview-text-two">
-        <h2>Novos uniformes disponíveis</h2>
-        <br/>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur a laudantium delectus, inventore laborum recusandae blanditiis illum sequi consequatur quia impedit ex dolor, odit voluptate exercitationem placeat nobis, dolore deleniti!</p>
+          <div className="newsletter-news">
+            <div className="newsletter-preview-text-two">
+              <h1>Novos uniformes disponíveis</h1>
+              <br />
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Consequuntur a laudantium delectus, inventore laborum recusandae
+                blanditiis illum sequi consequatur quia impedit ex dolor, odit
+                voluptate exercitationem placeat nobis, dolore deleniti!
+              </p>
+            </div>
+            <img src={Uniforme} className="newsletter-image"></img>
+          </div>
         </div>
-        <img src={Uniforme}className="newsletter-image"></img>
-        </div>
-
-        </div>
-
       </div>
-      
     </div>
   );
 }
